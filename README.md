64973715975e827be617b2d112ada11e6a3df7ec
ChristinaAzodi <azodichr@msu.edu>
2018-09-13 11:13:58 -0400

    Added Jaccard Index clustering and fixed bug of n=1

--- README.md
+++ README.md
@@ -25,8 +25,8 @@ module load OpenMPI/1.10.0
 module load R/3.3.2
 ```
 
-For some R scripts, additional packages are needed.
-    export R_LIBS_USER=/mnt/home/azodichr/R/library
+For some R scripts, additional packages are needed (all available on HPCC here:)
+```export R_LIBS_USER=/mnt/home/azodichr/R/library```
 
 
 ## 1. Get data
@@ -45,12 +45,6 @@ Soy:
 ## 1. Remove Duplicate SNPs
 
 ```
-module swap GNU GNU/4.9
-
-module load OpenMPI/1.10.0
-
-module load R/3.3.2
-
 Rscript ~/GitHub/ANN_GenBasis/remove_dups.R geno.csv
 ```
 Note: soy had 5 exact duplicates, maize had ??
@@ -63,12 +57,13 @@ Soy: Hold out 10% =
 
 Maize: Hold out 20% = 78 instances
 
-    python ~/GitHub/ML-Pipeline/holdout.py -df pheno.csv -sep ',' -type r -p 0.2<code/>
+```python ~/GitHub/ML-Pipeline/holdout.py -df pheno.csv -sep ',' -type r -p 0.2```
+
 
 ## 3. Feature Engineering/Selection
 
 ### PCA
-    Rscript ~/GitHub/ANN_GenBasis/make_feat_PCA.R geno_noDups.csv
+```Rscript ~/GitHub/ANN_GenBasis/make_feat_PCA.R geno_noDups.csv```
 
 ### MCA
 Packages needed: FactoMineR & factoextra
@@ -76,16 +71,54 @@ Packages needed: FactoMineR & factoextra
 Rscript -e "install.packages('FactoMineR', lib='~/R/library', contriburl=contrib.url('http://cran.r-project.org/'))"
 Rscript -e "install.packages('factoextra', lib='~/R/library', contriburl=contrib.url('http://cran.r-project.org/'))"
 ```
-    Rscript ~/GitHub/ANN_GenBasis/make_feat_MCA.R geno_noDups.csv
+```Rscript ~/GitHub/ANN_GenBasis/make_feat_MCA.R geno_noDups.csv```
+
+
 
 ### Clustering
-Packages needed: mpmi
-<code>Rscript -e "install.packages('mpmi', lib='~/R/library', contriburl=contrib.url('http://cran.r-project.org/'))"<code/>
+Packages needed: mpmi, cluster (pam only), vegan (jaccard - ji only)
+```
+Rscript -e "install.packages('mpmi', lib='~/R/library', contriburl=contrib.url('http://cran.r-project.org/'))"
+Rscript -e "install.packages('cluster', lib='~/R/library', contriburl=contrib.url('http://cran.r-project.org/'))
+Rscript -e "install.packages('vegan', lib='~/R/library', contriburl=contrib.url('http://cran.r-project.org/'))
+```
 
 #### PAM
-Packages needed: cluster
-<code>Rscript -e "install.packages('cluster', lib='~/R/library', contriburl=contrib.url('http://cran.r-project.org/'))<code/>
+```
+Rscript ~/GitHub/ANN_GenBasis/make_feat_Clust.R geno_noDups.csv pam 50
+```
 
-<code>Rscript ~/GitHub/ANN_GenBasis/make_feat_Clust.R geno_noDups.csv pam 50
-script ~/GitHub/ANN_GenBasis/make_feat_Clust.R geno_noDups.csv pam 500
-script ~/GitHub/ANN_GenBasis/make_feat_Clust.R geno_noDups.csv pam 1000<code/>
+#### Hierarchical clustering with Euclidean Distance
+```
+Rscript ~/GitHub/ANN_GenBasis/make_feat_Clust.R geno_noDups.csv hclus 50
+```
+
+#### Hierarchical clustering with Jaccard Index
+```
+Rscript ~/GitHub/ANN_GenBasis/make_feat_Clust.R geno_noDups.csv ji 50
+```
+
+* Example Submission:
+```python ~shius/codes/qsub_hpc.py -f submit -u azodichr -c run_clust.sh -w 800 -m 20 -J clust_soy -wd /mnt/home/azodichr/05_Insight/01_FeatureEngineering/sp_soy/```
+
+
+
+### Feature Selection
+Utilizing Feature Selection tool built in the ML_Pipeline from the [Shiu Lab] (https://github.com/ShiuLab/ML-Pipeline)
+Requirements: pandas, python3, scikit-learn
+Running on HPCC at MSU:
+```export PATH=/mnt/home/azodichr/miniconda/bin:$PATH```
+
+#### Random Forest
+```python ~/GitHub/ML-Pipeline/Feature_Selection.py -f RF -df geno_noDups.csv -df2 pheno.csv  -sep ',' -ho holdout.txt -y_name YLD -n 50,500,1000```
+
+#### Elastic Net
+```python ~/GitHub/ML-Pipeline/Feature_Selection.py -f EN -p 0.5 -df geno_noDups.csv -df2 pheno.csv  -sep ',' -ho holdout.txt -y_name YLD -n 50,500,1000```
+* Note: using L1:L2 ratio = 0.5 there were 2995 non-zero features for soy
+
+#### Relief
+Based on the rebate approach. See [here] (https://github.com/EpistasisLab/scikit-rebate) for info.
+```python ~/GitHub/ML-Pipeline/Feature_Selection.py -f relief -df geno_noDups.csv -df2 pheno.csv  -sep ',' -ho holdout.txt -y_name YLD -n 50,500,1000```
+
+
+
--- make_feat_Clust.R
+++ make_feat_Clust.R
@@ -2,11 +2,11 @@
 # Uses a feature file to run clustering
 #
 # Arguments: [1] input data
-#            [2] Clustering Method (pam, heir, ji, sbm)
+#            [2] Clustering Method (pam, hclus, ji, sbm)
 #            [3] k
 #
 # Written by: Christina Azodi
-# Original: 9.10.18
+# Original: 9.12.18
 # Modified: 
 #####################################
 
@@ -20,9 +20,10 @@ args <- commandArgs(TRUE)
 file <- args[1]
 clust_method <- args[2]
 k <- args[3]
-
+print(k)
 # Read in genotype data, convert to factor, and remove holdout
 g <- fread(file, sep=',', header=T)
+#g <- fread('geno_noDups.csv', sep=',', header=T)
 g <- as.data.frame(g)
 row.names(g) <- g$ID
 g$ID <- NULL
@@ -36,36 +37,108 @@ g_train <- t(g_train)
 
 
 
-library(cluster)
-m <- pam(g_train, k, metric='euclidean')
-df <- as.data.frame(m$clustering[m$id.med])
-names(df) <- c('k')
-df$mediod <- row.names(df)
-row.names(df) <- df$k
-df$k <- NULL
-clusts <- as.data.frame(m$clustering)
-names(clusts) <- c('k')
 
-# For each k ID the features, calculate mean PCC and mean MI
-pccs <- c()
-mis <- c()
+############
+### pam ####
+############
 
-for(ik in 1:k){
-  medio <- df[ik,'mediod']
-  all <- row.names(subset(clusts, k==ik))
-  cor <- cor(g[all])
-  cor[lower.tri(cor, diag=T)] <- NA
-  pccs <- c(pccs, summary(as.vector(abs(cor)))['Mean'])
+if(clust_method=='pam'){
+  print('Clustering with pam...')
+  library(cluster)
+  m <- pam(g_train, k, metric='euclidean')
+  df <- as.data.frame(m$clustering[m$id.med])
+  names(df) <- c('k')
+  df$mediod <- row.names(df)
+  row.names(df) <- df$k
+  df$k <- NULL
+  clusts <- as.data.frame(m$clustering)
+  names(clusts) <- c('k')
   
-  mi <- dmi(g[all])
-  mi$bcmi[lower.tri(mi$bcmi, diag=T)] <- NA
-  mis <- c(mis, summary(as.vector(abs(mi$bcmi)))['Mean'])
+  # For each k ID the features, calculate mean PCC and mean MI
+  print('Calculating cluster metrics...')
+  pccs <- c()
+  mis <- c()
+  ns <- m$clusinfo[,'size']
   
-  all <- all[all != medio]
-  df[ik,'group'] <- paste( unlist(all), collapse=',')
+  for(ik in 1:k){
+    medio <- df[ik,'mediod']
+    all <- row.names(subset(clusts, k==ik))
+    cor <- cor(g[all])
+    cor[lower.tri(cor, diag=T)] <- NA
+    pccs <- c(pccs, summary(as.vector(abs(cor)))['Mean'])
+    
+    mi <- dmi(g[all])
+    mi$bcmi[lower.tri(mi$bcmi, diag=T)] <- NA
+    mis <- c(mis, summary(as.vector(abs(mi$bcmi)))['Mean'])
+    
+    all <- all[all != medio]
+    df[ik,'group'] <- paste( unlist(all), collapse=',')
+  }
 }
 
-ns <- m$clusinfo[,'size']
+####################################
+### hclus: euclidean and Jaccard ###
+####################################
+
+if(clust_method=='hclus'){
+  print('Hierarchical clustering using Euclidean distance...')
+  d <- dist(g_train)
+}
+
+if(clust_method=='ji'){
+  print('Hierarchical clustering using Jaccard Index for distance...')
+  library(vegan)
+  g_train[g_train == -1] <- FALSE
+  g_train[g_train == 1] <- TRUE
+  d <- vegdist(g_train, method='jaccard', na.rm=TRUE, binary=TRUE)
+}
+
+
+# Function to find medoids from the distance matrix
+clust.medoid = function(i, distmat, clusters) {
+  ind = (clusters == i)
+  if(sum(ind)==1){
+    names(clusters[ind]) # Returns the only item in cluster if cluster size = 1
+  }else{
+    names(which.min(rowSums(distmat[ind, ind] )))
+  }
+}
+
+
+if(clust_method %in% c('hclus','ji')){
+  d2 <- as.matrix(d)
+  m <- cutree(hclust(d, method='ward.D'), k=k)
+  
+  # Put cluster assignments in clusts df
+  clusts <- as.data.frame(m)
+  names(clusts) <- c('k')
+  
+  # Pull out any clusters that are n=1:
+  df <- as.data.frame(sapply(unique(m), clust.medoid, d2, m))
+  names(df) <- c('mediod')
+
+  # For each k ID the features, calculate mean PCC and mean MI
+  print('Calculating cluster metrics...')
+  ns <- summary(as.factor(m))
+  pccs <- c()
+  mis <- c()
+  
+  for(ik in 1:k){
+    medio <- df[ik,'mediod']
+    all <- row.names(subset(clusts, k==ik))
+    cor <- cor(g[all])
+    cor[lower.tri(cor, diag=T)] <- NA
+    pccs <- c(pccs, summary(as.vector(abs(cor)))['Mean'])
+    
+    mi <- dmi(g[all])
+    mi$bcmi[lower.tri(mi$bcmi, diag=T)] <- NA
+    mis <- c(mis, summary(as.vector(abs(mi$bcmi)))['Mean'])
+    
+    all <- all[all != medio]
+    df[ik,'group'] <- paste( unlist(all), collapse=',')
+  }
+}
+
 
 
 # Output for each run
@@ -76,7 +149,17 @@ print(summary(mis))
 print('Clust size:')
 print(summary(ns))
 
-name <- paste('medio_', clust_method, '_', str(k), '.csv', sep = '')
-write.table(df$mediod, name, quote=F, col.names=F, row.names=F)
+sink('Clustering_Stats.csv')
+paste('Clustering results for', clust_method, 'with k=', k, '(PCC,MI,Num)', sep=' ')
+paste(summary(pccs), sep='')
+paste(summary(mis), sep='')
+paste(summary(ns), sep='')
+sink()
 
-      
+name1 <- paste('mediod_', clust_method, '_', k, '.csv', sep = '')
+write.table(df$mediod, name1, quote=F, col.names=F, row.names=F)
+name2 <- paste('clustAssign_', clust_method, '_', k, '.csv', sep = '')
+write.table(df, name2, sep=',', quote=F, col.names=F, row.names=F)
+
+
+print('Done!')
